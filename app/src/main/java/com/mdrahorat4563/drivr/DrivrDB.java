369bdc75f1736561b6b8c5230400ce1c0b510393
CONESTOGAC\mdrahorat4563 <michaldrahorat@gmail.com>
2017-10-05 15:50:52 -0400

    Changed buttons to imagebuttons

--- app/src/main/java/com/mdrahorat4563/drivr/DrivrDB.java
+++ app/src/main/java/com/mdrahorat4563/drivr/DrivrDB.java
@@ -3,6 +3,7 @@ package com.mdrahorat4563.drivr;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.provider.BaseColumns;
 import android.util.Log;
 
 /**
@@ -10,51 +11,18 @@ import android.util.Log;
  */
 
 public final class DrivrDB {
-    public static final String DB_NAME = "DrivrDB";
-    public static final int DB_VERSION = 1;
 
-    public static final String LOGIN_TABLE = "Login";
+    private DrivrDB() {}
 
-    //Declare the columns
-    public static final String LOGIN_ID = "_id";
-    public static final int LOGIN_ID_COL = 0;
-
-    public static final String LOGIN_USERNAME= "username";
-    public static final int LOGIN_NAME_COL = 1;
-
-    public static final String LOGIN_PASSWORD = "password";
-    public static final int LOGIN_PASSWORD_COL = 2;
-
-    public static final String CREATE_DB =
-            "CREATE DATABASE DrivrDB;";
-
-    public static final String CREATE_TABLES =
-            "CREATE TABLE " + LOGIN_TABLE + " (" +
-                    LOGIN_ID + " INTEGER PRIMARY KEY AUTO_INCREMENT, " +
-                    LOGIN_USERNAME + " TEXT NOT NULL UNIQUE," +
-                    LOGIN_PASSWORD + " TEXT NOT NULL);";
-
-    public void CreateDatabase(){
-
-    }
-
-    private class DBHelper extends SQLiteOpenHelper
+    public static class DrivrDBEntry implements BaseColumns
     {
+        public static final String LOGIN_TABLE = "Login";
 
-        public DBHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version){
-            super(context, name, factory, version);
-        }
+        //Declare the columns
+        public static final String LOGIN_ID = "_id";
 
-        public void onCreate(SQLiteDatabase db){
-            db.execSQL(CREATE_DB);
-            db.execSQL(CREATE_TABLES);
+        public static final String LOGIN_USERNAME= "username";
 
-            db.execSQL("INSERT INTO Logins VALUES (1, 'abc@123.com', 'abc123');");
-        }
-        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion){
-            Log.d("LoginDB", "Upgrading from v" + oldVersion + " to v" + newVersion);
-
-            db.execSQL("DROP TABLE Logins;");
-        }
+        public static final String LOGIN_PASSWORD = "password";
     }
 }
