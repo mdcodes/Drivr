96de95f63fe0868902a01ec688d107538ea1aebd
CONESTOGAC\mdrahorat4563 <michaldrahorat@gmail.com>
2017-11-29 09:49:23 -0500

    Made some fundamental changes to meet requirements

--- app/src/main/java/com/mdrahorat4563/drivr/AddPostActivity.java
+++ app/src/main/java/com/mdrahorat4563/drivr/AddPostActivity.java
@@ -67,7 +67,6 @@ public class AddPostActivity extends AppCompatActivity {
                     intent.putExtra("userId", userId);
                     finish();
                     startActivity(intent);
-
                 }
             }
         });
